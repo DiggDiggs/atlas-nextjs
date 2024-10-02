d687091ccf925bdfb66680ec93aec74a43efdc7c
Christopher Diggs <65854363+DiggDiggs@users.noreply.github.com>
2024-10-02 19:16:04 +0000

    auth-moddle-wear

--- app/ui/topic/[id]/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default async function Page({ params }: { params: { id: string } }) {
-  //Simulate Page loading
-  await new Promise((resolve) => setTimeout(resolve, 3000));
-  return <div>Topics Page: {params.id}</div>;
-}
